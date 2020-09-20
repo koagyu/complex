b958f821cfa5caf4a30f9779e74a1561a53c6440
Istvan Kohalmi <istvankohalmi@Istvans-iMac.local>
2020-09-20 18:56:34 +0200

    fix test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Other Page link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Other Page/i);
   expect(linkElement).toBeInTheDocument();
 });
